ea76010f2eb7dd174ed0a35d1b9646ad71494690
ferranbone <ferran.bone@enti.cat>
2025-02-19 11:03:27 +0100

    Cosas

---
+++ ejemplo/rollmania/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Dice from './Dice.js';
+
+function App() {
+  return (
+	<main className="App">
+	  <Dice />
+	  <Dice />
+	  <Dice />
+    </main>
+  );
+}
+
+export default App;
---
+++ ejemplo/rollmania/src/Die.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './Die.css';
+import { useState} from 'react';
+
+function Die() {
+	let [number, setNumber] = useState(1);
+
+	function roll ()
+	{
+		let r = Math.floor(Match.random() * 6) + 1
+		setNumber(r);
+	}
+  return (
+	<button className="Die" onClick={roll}>
+	  <p className="DieNumber">{number}</p>
+	</button>
+  );
+}
+
+export default Die;
