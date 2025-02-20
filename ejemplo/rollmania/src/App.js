f62aa171ac853268855c22abe708a1fb630e7c38
ferranbone <ferran.bone@enti.cat>
2025-02-20 22:00:52 +0100

    Update App.js

--- ejemplo/rollmania/src/App.js
+++ ejemplo/rollmania/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Dice from './Dice.js';
+import React, { useState } from "react";
 
 function App() {
   return (
@@ -9,6 +10,18 @@ function App() {
 	  <Dice />
 	  <Dice />
     </main>
+  );
+}
+
+function Contador() {
+  const [numero, setNumero] = useState(5);
+
+  return (
+    <section>
+      <button>Botón 1</button>
+      <button>Botón 2</button>
+      <p>{numero}</p>
+    </section>
   );
 }
 
