dde26d47782232d90da0ef9e4ac6d86f387582d9
ferranbone <ferran.bone@enti.cat>
2025-02-28 10:25:03 +0100

    Update Die.js

--- ejemplo/rollmania/src/Die.js
+++ ejemplo/rollmania/src/Die.js
@@ -1,19 +1,32 @@
-import logo from './logo.svg';
 import './Die.css';
-import { useState} from 'react';
 
-function Die() {
-	let [number, setNumber] = useState(1);
+import { useState } from 'react';
 
-	function roll ()
+
+function Die(props) {
+
+	let [number, setNumber] = useState("-");
+
+	function get_random ()
 	{
-		let r = Math.floor(Match.random() * 6) + 1
+		return Math.floor(Math.random() * 6) + 1
+	}
+
+	function set_random ()
+	{
+		let r = get_random();
 		setNumber(r);
 	}
+
+	if (props.roll){
+		number = get_random();
+	}
+
+
   return (
-	<button className="Die" onClick={roll}>
-	  <p className="DieNumber">{number}</p>
-	</button>
+    <button className="Die" onClick={set_random}>
+			<p className="DieNumber">{number}</p>
+    </button>
   );
 }
 
