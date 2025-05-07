66198641d33260649c7866dd43f002c0ada835a5
ferranbone <ferran.bone@enti.cat>
2025-05-07 17:25:47 +0200

    Update Dice.js

--- ejemplo/rollmania/src/Dice.js
+++ ejemplo/rollmania/src/Dice.js
@@ -1,24 +1,35 @@
+import { useRef } from 'react';
 import './Dice.css';
 import Die from './Die.js';
 
-function get_dice (quantity, roll) {
-	let dice_list = [];
+function Dice(props) {
+	const total = useRef(0);
+	const counter = useRef(0);
 
-	for (let i = 0; i < quantity; i++){
-		dice_list.push(<Die key={i} roll={roll} />);
+	function add_result (value) {
+		total.current += value;
+
+		counter.current++;
+		if (counter.current == props.cantidad){
+			props.onTotal(total.current);
+		}
 	}
 
-	return dice_list;
-}
+	function get_dice (quantity, roll) {
+		let dice_list = [];
 
+		for (let i = 0; i < quantity; i++){
+			dice_list.push(<Die key={i} roll={roll} onResult={add_result} />);
+		}
 
-function Dice(props) {
+		return dice_list;
+	}
+
 	return (
 		<div className="Dice">
 			{get_dice(props.cantidad, props.roll)}
 		</div>
 	);
-
 }
 
 export default Dice;
